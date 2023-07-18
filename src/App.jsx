1f910fa1e3569aba5d21d01ce897fbe9fa953b27
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-07-18 16:18:14 +0600

    testing push

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <div className="bg-slate-500 h-80 items-center flex justify-center flex-col font-extrabold text-4xl text-white rounded-3xl border border-black hover:text-red-400 cursor-pointer"> 
         <h1>Name : Mursalin Hossain</h1>
         <h1>Name : Put ur name here...</h1>
+        <p>Second Name : Abdullah Al Noman</p>
       </div>
     </>
   );
